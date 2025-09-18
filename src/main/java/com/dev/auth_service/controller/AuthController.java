b4873c721afef779994717e31d15b3d6144b325a
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-18 13:49:05 -0300

    Update AuthController.java
    - Alterado a injeção para que utilize o final e adicionado a anotação @RequiredArgsConstructor para a criação do construtor automaticamente.
    - No metodo login adicionei a anotação Valid para tratar os campos do Dto.
    - No metodo register alterei a anotação de @Validated para @Valid e o arrumei o retorno para o status code correto.

--- src/main/java/com/dev/auth_service/controller/AuthController.java
+++ src/main/java/com/dev/auth_service/controller/AuthController.java
@@ -4,36 +4,34 @@ import com.dev.auth_service.dto.TokenDTO;
 import com.dev.auth_service.dto.UserRequestDTO;
 import com.dev.auth_service.dto.UserResponseDTO;
 import com.dev.auth_service.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserService authenticationService;
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    public AuthController(UserService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDTO> login(@RequestBody UserRequestDTO dto){
+    public ResponseEntity<TokenDTO> login(@RequestBody @Valid UserRequestDTO dto){
         return ResponseEntity.ok(new TokenDTO(authenticationService.authenticate(dto)));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO> register(@RequestBody @Validated UserRequestDTO dto){
-        return ResponseEntity.status(HttpStatus.OK).body(userService.register(dto));
+    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserRequestDTO dto){
+    	UserResponseDTO userCreated = userService.register(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
